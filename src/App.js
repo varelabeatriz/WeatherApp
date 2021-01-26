e7796f7a5b99e01c9438e62fc8672928dcc53c06
Beatriz Varela <beatrizsv19@gmail.com>
2021-01-26 16:19:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { Fragment, useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        // lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+    console.log(res.data);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, []);
+
+  if (location == false) {
+    return (
+      <Fragment>
+        Habilite a localização no browser!
+      </Fragment>
+    )
+  } else if (weather == false) {
+    return (
+      <Fragment>
+        Carregando...
+      </Fragment>
+    )
+  }
+  else {
+
+    let icon = weather['weather'][0]['icon']
+
+    return (
+      <Fragment>
+        <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: {weather['main']['temp']}°</li>
+          <li>Temperatura máxima: {weather['main']['temp_max']}°</li>
+          <li>Temperatura mínima: {weather['main']['temp_min']}°</li>
+          <li>Pressão: {weather['main']['pressure']}°</li>
+          <li>Umidade: {weather['main']['humidity']}%</li>
+          <img src= {"https://openweathermap.org/img/w/" +icon+ ".png"} style={{width: 80}}></img>
+          {/* <li>Umidade: {weather['daily'][1]}%</li> */}
+        </ul>
+      </Fragment>
+    );
+  }
 }
+
 
 export default App;
